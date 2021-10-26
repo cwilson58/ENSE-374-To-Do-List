d2fbd97a339465a92205fd6594955c3fb7914f49
Cameron Wilson <caw522@uregina.ca>
2021-10-26 11:04:28 -0600

    clean up code for lab 7

--- index.js
+++ index.js
@@ -1,188 +1,180 @@
 const { json } = require("express");
-const express = require ( "express" );
-const fs = require('fs');
-const readTasks = fs.readFileSync("ejs-eg/tasks.json");
-var taskString = JSON.parse(readTasks);
+const express = require("express");
+const fs = require("fs");
+var taskString = JSON.parse(fs.readFileSync("ejs-eg/tasks.json"));
 var userString = JSON.parse(fs.readFileSync("ejs-eg/users.json"));
 
-
 // this is a canonical alias to make your life easier, like jQuery to $.
-const app = express(); 
-app.use(express.urlencoded({ extended: true})); 
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 // a common localhost test port
-const port = 3000; 
+const port = 3000;
 
 app.use(express.static("ejs-eg"));
 
 app.set("view engine", "ejs");
 
-
-let users = {
-    "listOfAllUsers" : [
-        {username: "username", password: "password"},
-        {username: "potato", password: "mashed"}
-    ]
-};
 var currUsersName = "";
-function renderToDoPage(res){
-    res.render("ToDo", {username: currUsersName, itemList: taskString.listOfAllTasks});
+function renderToDoPage(res) {
+  res.render("ToDo", {
+    username: currUsersName,
+    itemList: taskString.listOfAllTasks,
+  });
+  return;
 }
-function saveNewUser(newUsername, newPassword){
-    userString.listOfAllUsers.push({
-        username: newUsername,
-        password: newPassword
-    });
-    fs.writeFile("ejs-eg/users.json", JSON.stringify(userString),function(err){
-        if(err){
-            console.log("error");
-        }
-        else{
-            console.log("WRITTEN a new user!");
-        }
-    });
+function findStartingId() {
+  if (
+    taskString.listOfAllTasks[taskString.listOfAllTasks.length - 1]._id <
+    taskString.listOfAllTasks.length
+  ) {
+    return taskString.listOfAllTasks.length;
+  } else {
+    return (
+      taskString.listOfAllTasks[taskString.listOfAllTasks.length - 1]._id + 1
+    );
+  }
+}
+function saveNewUser(newUsername, newPassword) {
+  userString.listOfAllUsers.push({
+    username: newUsername,
+    password: newPassword,
+  });
+  fs.writeFile("ejs-eg/users.json", JSON.stringify(userString), function (err) {
+    if (err) {
+      console.log("error");
+    } else {
+      console.log("WRITTEN a new user!");
+    }
+  });
+  return;
+}
+function writeTaskToFile(res) {
+  fs.writeFile("ejs-eg/tasks.json", JSON.stringify(taskString), function (err) {
+    if (err) {
+      console.log("error");
+    } else {
+      renderToDoPage(res);
+    }
+  });
+  return;
 }
 //user registration
-app.post("/register", (req,res)=>{
-    //Make a user
-    //Save its username and password to the json file after making sure the inputs are correct
-    var validRegistration = true;
-    userString.listOfAllUsers.forEach(user =>{
-        if(req.body.email === user.username){
-            validRegistration = false;
-        }
-    });
-    if(validRegistration && req.body.auth === "todo2021"){
-        currUsersName = req.body.email;
-        saveNewUser(currUsersName, req.body.passowrd);
-        renderToDoPage(res);
+app.post("/register", (req, res) => {
+  var validRegistration = true;
+  userString.listOfAllUsers.forEach((user) => {
+    if (req.body.email === user.username) {
+      validRegistration = false;
     }
-    else{
-        console.log("Fail");
-    }
+  });
+  if (validRegistration && req.body.auth === "todo2021") {
+    currUsersName = req.body.email;
+    saveNewUser(currUsersName, req.body.passowrd);
+    renderToDoPage(res);
+    return;
+  } else {
+    console.log("Fail");
+  }
+  return;
 });
 //Checking username and password
-app.post("/login", (req, res)=>{
-    //read the json file into an array
-    users.listOfAllUsers.forEach(user =>{
-        if(req.body.Email === user.username && req.body.password === user.password){
-            currUsersName = req.body.Email;
-            renderToDoPage(res);
-        }
-    })
+app.post("/login", (req, res) => {
+  //read the json file into an array
+  userString.listOfAllUsers.forEach((user) => {
+    if (
+      req.body.Email === user.username &&
+      req.body.password === user.password
+    ) {
+      currUsersName = req.body.Email;
+      renderToDoPage(res);
+      return;
+    }
+  });
 });
 //Add task to the to do list
 
-var idCounter = taskString.listOfAllTasks.length;
-app.post("/addtask", (req, res)=>{
-    
-    var newTask = req.body.taskInput;
-    taskString.listOfAllTasks.push( {
-        _id: idCounter,
-        text: newTask,
-        state: "unclaimed",
-        creator: currUsersName,
-        isTaskClaimed: false,
-        claimingUser: null,
-        isTaskDone: false,
-        isTaskCleared: false
-    });
-    idCounter++;
-    fs.writeFile("ejs-eg/tasks.json", JSON.stringify(taskString),function(err){
-        if(err){
-            console.log("error");
-        }
-        else{
-            renderToDoPage(res);
-        }
-    });
+var idCounter = findStartingId();
+app.post("/addtask", (req, res) => {
+  var newTask = req.body.taskInput;
+  taskString.listOfAllTasks.push({
+    _id: idCounter,
+    text: newTask,
+    state: "unclaimed",
+    creator: currUsersName,
+    isTaskClaimed: false,
+    claimingUser: null,
+    isTaskDone: false,
+    isTaskCleared: false,
+  });
+  idCounter++;
+  writeTaskToFile(res);
+  return;
 });
 
-app.post("/claim", (req,res)=>{
-    idToClaim = req.body.hiddenId;
-    taskString.listOfAllTasks.forEach(task =>{
-        if(task._id == idToClaim){
-            task.state = "claimed";
-            task.isTaskClaimed = true;
-            task.claimingUser = currUsersName;
-            fs.writeFile("ejs-eg/tasks.json", JSON.stringify(taskString),function(err){
-                if(err){
-                    console.log("error");
-                }
-                else{
-                    renderToDoPage(res);
-                }
-            });
-        }
-    });
-});
-
-app.post("/abandon", (req,res)=>{
-    idToClaim = req.body.hiddenId;
-    taskString.listOfAllTasks.forEach(task =>{
-        if(task._id == idToClaim ){
-            if(req.body.Task === "true"){
-                task.state = "finished";
-                task.isTaskDone = true;
-                
-            }
-            else{
-                task.state = "unclaimed";
-                task.isTaskClaimed = false;
-                task.claimingUser = null;
-            }
-            
-            fs.writeFile("ejs-eg/tasks.json", JSON.stringify(taskString),function(err){
-                if(err){
-                    console.log("error");
-                }
-                else{
-                    renderToDoPage(res);
-                }
-            });
-        }
-        
-    });
-});
-app.post("/unfinish", (req,res)=>{
-    var idToUnfin = req.body.hiddenId;
-    taskString.listOfAllTasks.forEach(task =>{
-        if(task._id == idToUnfin ){
-            task.state = "claimed";
-            task.isTaskClaimed = true;
-            task.isTaskDone = false;
-        }
-    });
-    fs.writeFile("ejs-eg/tasks.json", JSON.stringify(taskString),function(err){
-        if(err){
-            console.log("error");
-        }
-        else{
-            renderToDoPage(res);
-        }
-    });
-});
-app.post("/purge",(req,res) =>{
-    for(var i = 0; i < taskString.listOfAllTasks.length; i++){
-        if(taskString.listOfAllTasks[i].isTaskDone){
-            taskString.listOfAllTasks.splice(i,1);
-            i--;
-        }
+app.post("/claim", (req, res) => {
+  idToClaim = req.body.hiddenId;
+  taskString.listOfAllTasks.forEach((task) => {
+    if (task._id == idToClaim) {
+      task.state = "claimed";
+      task.isTaskClaimed = true;
+      task.claimingUser = currUsersName;
     }
-    fs.writeFile("ejs-eg/tasks.json", JSON.stringify(taskString),function(err){
-        if(err){
-            console.log("error");
-        }
-        else{
-            renderToDoPage(res);
-        }
-    });
+  });
+  writeTaskToFile(res);
+  return;
 });
-app.post("/logout", (req,res) =>{
-    res.redirect("../index.html");
+
+app.post("/abandon", (req, res) => {
+  idToClaim = req.body.hiddenId;
+  taskString.listOfAllTasks.forEach((task) => {
+    if (task._id == idToClaim) {
+      if (req.body.Task === "true") {
+        task.state = "finished";
+        task.isTaskDone = true;
+      } else {
+        task.state = "unclaimed";
+        task.isTaskClaimed = false;
+        task.claimingUser = null;
+      }
+    }
+  });
+  writeTaskToFile(res);
+  return;
+});
+app.post("/unfinish", (req, res) => {
+  var idToUnfin = req.body.hiddenId;
+  taskString.listOfAllTasks.forEach((task) => {
+    if (task._id == idToUnfin) {
+      task.state = "claimed";
+      task.isTaskClaimed = true;
+      task.isTaskDone = false;
+    }
+  });
+  writeTaskToFile(res);
+  return;
+});
+app.post("/purge", (req, res) => {
+  for (var i = 0; i < taskString.listOfAllTasks.length; i++) {
+    if (taskString.listOfAllTasks[i].isTaskDone) {
+      taskString.listOfAllTasks.splice(i, 1);
+      i--;
+    }
+  }
+  fs.writeFile("ejs-eg/tasks.json", JSON.stringify(taskString), function (err) {
+    if (err) {
+      console.log("error");
+    } else {
+      renderToDoPage(res);
+    }
+  });
+  return;
+});
+app.post("/logout", (req, res) => {
+  res.redirect("../index.html");
+  return;
 });
 // Simple server operation
-app.listen (port, () => {
-    // template literal
-    console.log (`Server is running on http://localhost:${port}`);
+app.listen(port, () => {
+  // template literal
+  console.log(`Server is running on http://localhost:${port}`);
 });
