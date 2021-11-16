9bdd7d3f8e95666414dbcc2f73fbfd8ca93af0c2
Cameron Wilson <caw522@uregina.ca>
2021-11-16 12:21:06 -0600

    add mongodb functionality

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 /*
 Cameron Wilson
 SID: 200430766
-index.js for ENSE-374 lab 7 due on 10/27/2021
+index.js for ENSE-374 labs
 */
-const { json } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
-const fs = require("fs");
-const { kStringMaxLength } = require("buffer");
-const internal = require("stream");
-var taskString = JSON.parse(fs.readFileSync("ejs-eg/tasks.json"));
-var userString = JSON.parse(fs.readFileSync("ejs-eg/users.json"));
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+require("dotenv").config();
 
 // this is a canonical alias to make your life easier, like jQuery to $.
 const app = express();
@@ -25,6 +23,17 @@ mongoose.connect("mongodb://localhost:27017/toDoList", {
 });
 
 app.use(express.static("ejs-eg"));
+// 2. Create a session. The secret is used to sign the session ID.
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("view engine", "ejs");
 
@@ -32,6 +41,7 @@ const userSchema = new mongoose.Schema({
   username: String,
   password: String,
 });
+userSchema.plugin(passportLocalMongoose);
 
 const taskSchema = new mongoose.Schema({
   _id: Number,
@@ -43,19 +53,23 @@ const taskSchema = new mongoose.Schema({
   isTaskDone: Boolean,
   isTaskCleared: Boolean,
 });
-const Users = mongoose.model("Users", userSchema);
-const Tasks = mongoose.model("Tasks", taskSchema);
+const Users = new mongoose.model("Users", userSchema);
+const Tasks = new mongoose.model("Tasks", taskSchema);
 
+// 4. Add our strategy for using Passport, using the local user from MongoDB
+passport.use(Users.createStrategy());
+
+passport.serializeUser(Users.serializeUser());
+passport.deserializeUser(Users.deserializeUser());
+//Make and save test users and tasks for the db.
 const user1 = new Users({
   username: "username",
   password: "password",
 });
-//user1.save();
 const user2 = new Users({
   username: "potato",
   password: "mashed",
 });
-//user2.save();
 const task1 = new Tasks({
   _id: 1,
   text: "Get Milk",
@@ -66,7 +80,6 @@ const task1 = new Tasks({
   isTaskDone: false,
   isTaskCleared: false,
 });
-//task1.save();
 const task2 = new Tasks({
   _id: 2,
   text: "Sleep",
@@ -77,7 +90,6 @@ const task2 = new Tasks({
   isTaskDone: false,
   isTaskCleared: false,
 });
-//task2.save();
 const task3 = new Tasks({
   _id: 3,
   text: "Do ENSE 374 Lab 7",
@@ -88,7 +100,6 @@ const task3 = new Tasks({
   isTaskDone: false,
   isTaskCleared: false,
 });
-//task3.save();
 const task4 = new Tasks({
   _id: 4,
   text: "Pay Attention in class",
@@ -99,7 +110,6 @@ const task4 = new Tasks({
   isTaskDone: false,
   isTaskCleared: false,
 });
-//task4.save();
 const task5 = new Tasks({
   _id: 5,
   text: "Drink more coffee",
@@ -110,171 +120,151 @@ const task5 = new Tasks({
   isTaskDone: false,
   isTaskCleared: false,
 });
+//user1.save();
+//user2.save();
+//task1.save();
+//task2.save();
+//task3.save();
+//task4.save();
 //task5.save();
-
-var currUsersName = "";
-function renderToDoPage(res) {
-  res.render("ToDo", {
-    username: currUsersName,
-    itemList: taskString.listOfAllTasks,
-  });
-  return;
-}
-function findStartingId() {
-  if (
-    taskString.listOfAllTasks[taskString.listOfAllTasks.length - 1]._id <
-    taskString.listOfAllTasks.length
-  ) {
-    return taskString.listOfAllTasks.length;
-  } else {
-    return (
-      taskString.listOfAllTasks[taskString.listOfAllTasks.length - 1]._id + 1
-    );
-  }
-}
-function saveNewUser(newUsername, newPassword) {
-  userString.listOfAllUsers.push({
-    username: newUsername,
-    password: newPassword,
-  });
-  fs.writeFile("ejs-eg/users.json", JSON.stringify(userString), function (err) {
+function renderToDoPage(req, res) {
+  Tasks.find((err, tasks) => {
     if (err) {
-      console.log("error");
+      console.log(err);
     } else {
-      console.log("WRITTEN a new user!");
+      res.render("ToDo", {
+        username: req.user.username,
+        itemList: tasks,
+      });
     }
   });
   return;
 }
-function writeTaskToFile(res) {
-  fs.writeFile("ejs-eg/tasks.json", JSON.stringify(taskString), function (err) {
-    if (err) {
-      console.log("error");
-    } else {
-      renderToDoPage(res);
+//updated to work with db and async
+async function findStartingId() {
+  var nextAVailId = 0;
+  let taskIds = await Tasks.find().distinct("_id");
+  taskIds.forEach((element) => {
+    if (parseInt(element) >= nextAVailId) {
+      nextAVailId = parseInt(element) + 1;
     }
   });
-  return;
+  return nextAVailId;
 }
 //user registration
 app.post("/register", (req, res) => {
-  var validRegistration = true;
-  userString.listOfAllUsers.forEach((user) => {
-    if (req.body.email === user.username) {
-      validRegistration = false;
+  console.log("User " + req.body.username + " is attempting to register");
+  Users.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          renderToDoPage(req, res);
+        });
+      }
     }
-  });
-  if (validRegistration && req.body.auth === "todo2021") {
-    currUsersName = req.body.email;
-    saveNewUser(currUsersName, req.body.passowrd);
-    renderToDoPage(res);
-    return;
-  } else {
-    console.log("Fail");
-  }
-  return;
+  );
 });
 //user login
 app.post("/login", (req, res) => {
-  //read the json file into an array
-  userString.listOfAllUsers.forEach((user) => {
-    if (
-      req.body.Email === user.username &&
-      req.body.password === user.password
-    ) {
-      currUsersName = req.body.Email;
-      renderToDoPage(res);
-      return;
+  console.log("User " + req.body.username + " Is attempting to log in!");
+  const user = new Users({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/");
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        renderToDoPage(req, res);
+      });
     }
   });
 });
 //add a task to the list
-var idCounter = findStartingId();
-app.post("/addtask", (req, res) => {
-  var newTask = req.body.taskInput;
-  taskString.listOfAllTasks.push({
+app.post("/addtask", async (req, res) => {
+  var idCounter = await findStartingId(); //wait until we have found an id
+  console.log("Adding a new task! with id " + idCounter);
+  const newTask = new Tasks({
     _id: idCounter,
-    text: newTask,
+    text: req.body.taskInput,
     state: "unclaimed",
-    creator: currUsersName,
+    creator: req.user.username,
     isTaskClaimed: false,
     claimingUser: null,
-    isTaskDone: false,
+    isTaskdone: false,
     isTaskCleared: false,
   });
-  idCounter++;
-  writeTaskToFile(res);
+  //once the save is done, reRenderThePage
+  newTask.save().then(() => {
+    console.log("Save complete!");
+    renderToDoPage(req, res);
+  });
   return;
 });
 //claim a task
-app.post("/claim", (req, res) => {
+app.post("/claim", async (req, res) => {
   idToClaim = req.body.hiddenId;
-  taskString.listOfAllTasks.forEach((task) => {
-    if (task._id == idToClaim) {
-      task.state = "claimed";
-      task.isTaskClaimed = true;
-      task.claimingUser = currUsersName;
-    }
-  });
-  writeTaskToFile(res);
+  usersName = req.user.username;
+  //find the id in the Tasks collection
+  //update the state to claimed
+  //update claiming user to req.user.username
+  await Tasks.findOneAndUpdate(
+    { _id: idToClaim },
+    { state: "claimed", claimingUser: usersName, isTaskClaimed: true }
+  );
+  renderToDoPage(req, res);
   return;
 });
 //abandon and complete
-app.post("/abandon", (req, res) => {
-  idToClaim = req.body.hiddenId;
-  taskString.listOfAllTasks.forEach((task) => {
-    if (task._id == idToClaim) {
-      if (req.body.Task === "true") {
-        task.state = "finished";
-        task.isTaskDone = true;
-      } else {
-        task.state = "unclaimed";
-        task.isTaskClaimed = false;
-        task.claimingUser = null;
-      }
-    }
-  });
-  writeTaskToFile(res);
+app.post("/abandon", async (req, res) => {
+  var idToAbandon = req.body.hiddenId;
+  if (req.body.Task === "true") {
+    console.log("Finish id: " + idToAbandon);
+    await Tasks.findOneAndUpdate(
+      { _id: idToAbandon },
+      { state: "finished", isTaskDone: true }
+    );
+  } else {
+    console.log("Abandon id: " + idToAbandon);
+    await Tasks.findOneAndUpdate(
+      { _id: idToAbandon },
+      { state: "unclaimed", claimingUser: null, isTaskClaimed: false }
+    );
+  }
+  renderToDoPage(req, res);
   return;
 });
 //mark a task as not actually finished
-app.post("/unfinish", (req, res) => {
+app.post("/unfinish", async (req, res) => {
   var idToUnfin = req.body.hiddenId;
-  taskString.listOfAllTasks.forEach((task) => {
-    if (task._id == idToUnfin) {
-      task.state = "claimed";
-      task.isTaskClaimed = true;
-      task.isTaskDone = false;
-    }
-  });
-  writeTaskToFile(res);
+  await Tasks.findOneAndUpdate(
+    { _id: idToUnfin },
+    { state: "claimed", isTaskDone: false, isTaskClaimed: true }
+  );
+  renderToDoPage(req, res);
   return;
 });
 //purge all completed task: Note this does not care about who the user is
-app.post("/purge", (req, res) => {
-  for (var i = 0; i < taskString.listOfAllTasks.length; i++) {
-    if (taskString.listOfAllTasks[i].isTaskDone) {
-      taskString.listOfAllTasks.splice(i, 1);
-      i--;
-    }
-  }
-  fs.writeFile("ejs-eg/tasks.json", JSON.stringify(taskString), function (err) {
-    if (err) {
-      console.log("error");
-    } else {
-      renderToDoPage(res);
-    }
-  });
+app.post("/purge", async (req, res) => {
+  await Tasks.deleteMany({ state: "finished" });
+  renderToDoPage(req, res);
   return;
 });
-app.post("/logout", (req, res) => {
-  res.redirect("../index.html");
-  return;
+app.get("/logout", (req, res) => {
+  console.log("A user is logging out");
+  req.logout();
+  res.redirect("/");
 });
 // Simple server operation
 app.listen(port, () => {
   // template literal
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-mongoose.connection.close();
+//mongoose.connection.close();
